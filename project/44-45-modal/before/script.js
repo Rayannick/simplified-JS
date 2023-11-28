ff972d9c93bbfe7b468850cf2807718b368b02d4
Kibrya <efte7123@gmail.com>
2023-11-28 16:50:26 +0600

    half

--- project/44-45-modal/before/script.js
+++ project/44-45-modal/before/script.js
@@ -5,8 +5,19 @@
     * close-modal-btn
     * BONUS: overlay
 */
+const modal =document.querySelector('#modal')
+const openM =document.querySelector('#open-modal-btn')
+const closeM =document.querySelector('#close-modal-btn')
+
 
 // TODO: 3. Create a click event listener for the open-modal-btn that adds the class "open" to the modal
+openM.addEventListener('click',()=>{
+  modal.classList.add('open')
+})
+
+
+
+
 // BONUS: Also add the class "open" to the overlay
 
 // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
