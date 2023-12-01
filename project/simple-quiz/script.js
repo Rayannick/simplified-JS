cec4ae894fb882fa1c644038aa5eba62e21576cf
Kibrya <efte7123@gmail.com>
2023-12-01 21:49:24 +0600

    quiz complete

--- project/simple-quiz/script.js
+++ project/simple-quiz/script.js
@@ -6,9 +6,53 @@
     * BONUS: The alert (has the id `alert`)
 */
 
+const form = document.getElementById('quiz-form')
+const answers =Array.from(document.querySelectorAll('.answer')) //query gives us string/ irretable obj. it will return an array for filter method
+const questions = document.querySelectorAll('.question-item')
+const aalert = document.getElementById('alert')
+
+
+
+
+
+
+
 // TODO: 3. Create a submit event listener for the form that does the following.
-//    1. Prevent the default behaviour
+form.addEventListener('submit',(e)=>{
+
+
+  //    1. Prevent the default behaviour
+e.preventDefault()
 //    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
+
+
+questions.forEach(n => {
+  n.classList.add('incorrect')
+})
+
+
+const checkedAns = answers.filter(n => n.checked)
+checkedAns.forEach(n=> {
+  const checkedQuestion = n.closest('.question-item')
+
+  if (n.value === "true"){
+    checkedQuestion.classList.add('correct')
+    checkedQuestion.classList.remove('incorrect')
+  }else{
+    checkedQuestion.classList.add('incorrect')
+  }
+  const allTrue = checkedAns.every(answer => answer.value ==='true')// return boolean
+  const allAnswered = checkedAns.length === questions.length
+
+  if(allTrue && allAnswered) {
+    aalert.classList.add('active')
+    setTimeout(function(){
+      aalert.classList.remove('active')
+    },3000)
+  }
+})
+})
+  
 //    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
 //    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
 //    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
