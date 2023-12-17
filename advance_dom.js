166ac8f730d38e35afa1e95d8350903a178c9dff
Kibrya <efte7123@gmail.com>
2023-12-18 02:40:29 +0600

    update AD

--- advance_dom.js
+++ advance_dom.js
@@ -1,48 +1,48 @@
-const URL1 = 'https://jsonplaceholder.typicode.com/users';
-const button = document.querySelector('.button')
-const button2 = document.querySelector('.button2')
+const URL1 = 'https://jsonplaceholder.typicode.com/posts';
+// const button = document.querySelector('.button')
+// const button2 = document.querySelector('.button2')
 const button3 = document.querySelector('.button3')
 
-button.addEventListener('click',doStuff)
-button2.addEventListener('click',doStuff2)
+// button.addEventListener('click',doStuff)
+// button2.addEventListener('click',doStuff2)
 button3.addEventListener('click',doStuff3)
 
 
 
 
-// Getting data through api
+// // Getting data through api
 
 
 
 
-//  method 1
-function doStuff(){
-fetch(URL1).then(resp =>{
-    return resp.json()
-}).then(data =>{
-    console.log(data)
-})
+// //  method 1
+// function doStuff(){
+// fetch(URL1).then(resp =>{
+//     return resp.json()
+// }).then(data =>{
+//     console.log(data.map(user => user.name))
+// })
 
-}
+// }
 
-// method 2
-async function doStuff2(){ 
-    try{
-        const response = await fetch(URL1)
-        if(response.ok){
-            // notice the difference
+// // method 2
+// async function doStuff2(){ 
+//     try{
+//         const response = await fetch(URL1)
+//         if(response.ok){
+//             // notice the difference
 
-            // const users = response.json()
-            const users = await response.json()
-            console.log(users)
-        }else{
-            console.log('kon baler user?? Database e nai') // it will give error for the api
-        }
+//             // const users = response.json()
+//             const users = await response.json()
+//             console.log(users)
+//         }else{
+//             console.log('kon baler user?? Database e nai') // it will give error for the api
+//         }
     
-    }catch(e){
-        console.log('errrrrarr') //it will give error for the function
-    }
- }
+//     }catch(e){
+//         console.log('errrrrarr') //it will give error for the function
+//     }
+//  }
 
 
 
@@ -57,7 +57,7 @@ async function doStuff3(){
             
             "Content-type": 'application/json'
         },
-        body:JSON.stringify({
+        body: JSON.stringify({
             title: 'New POSssssssssssssssst'
         })
     })
