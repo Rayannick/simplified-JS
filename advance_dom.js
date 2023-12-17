e466713911fd56fa87f47e488551f1b74f93558b
Kibrya <efte7123@gmail.com>
2023-12-18 01:19:12 +0600

    update AD

--- advance_dom.js
+++ advance_dom.js
@@ -1,3 +1,22 @@
-fetch('https://jsonplaceholder.typicode.com/users').then(res =>{
-    console.log(res)
-})
+const URL1 = 'https://jsonplaceholder.typicode.com/users';
+const button = document.querySelector('button')
+
+button.addEventListener('click',doStuff)
+
+async function doStuff(){ 
+    try{
+        const response = await fetch(URLd1)
+        if(response.ok){
+            // notice the difference
+
+            // const users = response.json()
+            const users = await response.json()
+            console.log(users.name)
+        }else{
+            console.log('kon baler user?? Database e nai') // it will give error for the api
+        }
+    
+    }catch(e){
+        console.log('errrrrarr') //it will give error for the function
+    }
+ }
