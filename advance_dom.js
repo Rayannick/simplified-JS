ab181ca04e4594f837621cbb583cfbdc0a828211
Kibrya <efte7123@gmail.com>
2023-12-18 02:08:40 +0600

    update AD

--- advance_dom.js
+++ advance_dom.js
@@ -1,17 +1,40 @@
 const URL1 = 'https://jsonplaceholder.typicode.com/users';
-const button = document.querySelector('button')
+const button = document.querySelector('.button')
+const button2 = document.querySelector('.button2')
+const button3 = document.querySelector('.button3')
 
 button.addEventListener('click',doStuff)
+button2.addEventListener('click',doStuff2)
+button3.addEventListener('click',doStuff3)
 
-async function doStuff(){ 
+
+
+
+// Getting data through api
+
+
+
+
+//  method 1
+function doStuff(){
+fetch(URL1).then(resp =>{
+    return resp.json()
+}).then(data =>{
+    console.log(data)
+})
+
+}
+
+// method 2
+async function doStuff2(){ 
     try{
-        const response = await fetch(URLd1)
+        const response = await fetch(URL1)
         if(response.ok){
             // notice the difference
 
             // const users = response.json()
             const users = await response.json()
-            console.log(users.name)
+            console.log(users)
         }else{
             console.log('kon baler user?? Database e nai') // it will give error for the api
         }
@@ -20,3 +43,24 @@ async function doStuff(){
         console.log('errrrrarr') //it will give error for the function
     }
  }
+
+
+
+
+
+// Posting data through api
+
+async function doStuff3(){
+    const response = await fetch(URL1,{
+        method: "POST",
+        headers:{
+            
+            "Content-type": 'application/json'
+        },
+        body:JSON.stringify({
+            title: 'New POSssssssssssssssst'
+        })
+    })
+    const post = await response.json()
+    console.log(post)
+}
