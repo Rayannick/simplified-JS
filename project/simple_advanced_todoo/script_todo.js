347125b56d2ee31d563bf683535d60efbbce0d86
Kibrya <efte7123@gmail.com>
2023-12-22 23:47:29 +0600

    update

--- project/simple_advanced_todoo/script_todo.js
+++ project/simple_advanced_todoo/script_todo.js
@@ -1,1 +1,10 @@
-console.log('vala')
+const form = document.querySelector("#new-todo-form")
+const input = document.querySelector("#todo-input")
+
+
+
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+})
