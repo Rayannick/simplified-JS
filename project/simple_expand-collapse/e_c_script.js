aa45866e8786b7b1cb485bf943321f62cfb99f66
Kibrya <efte7123@gmail.com>
2023-12-27 19:18:21 +0600

    update

--- project/simple_expand-collapse/e_c_script.js
+++ project/simple_expand-collapse/e_c_script.js
@@ -1,4 +1,7 @@
 document.addEventListener('click', (e)=>{
     if(!e.target.matches('.expand-button')) return
+    const card =e.target.closest('.card')
+    const cardBody = card.querySelector('.card-body')
      
+    cardBody.classList.toggle('show')
 })
