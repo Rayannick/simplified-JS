5ad83ef97f623ac3e2e44b4ea337f436de09f8d6
Kibrya <efte7123@gmail.com>
2024-01-09 23:56:02 +0600

    update

--- project/simple_date-picker/script.js
+++ project/simple_date-picker/script.js
@@ -1,2 +1,4 @@
 const datePickerButton = document.querySelector('.date-picker-button')
 const datePicker = document.querySelector('.date-picker')
+const datePickerHeaderText = document.querySelector('.current-month')
+const previousMonthButton = document.querySelector()
