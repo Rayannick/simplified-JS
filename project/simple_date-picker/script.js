1f7386ac7bd2020332129373e213eed6f93ed34b
Kibrya <efte7123@gmail.com>
2024-01-10 19:14:30 +0600

    update

--- project/simple_date-picker/script.js
+++ project/simple_date-picker/script.js
@@ -1,3 +1,5 @@
+import { addMonths, format, fromUnixTime, getUnixTime, subMonths } from "date-fns"
+
 const datePickerButton = document.querySelector('.date-picker-button')
 const datePicker = document.querySelector('.date-picker')
 const datePickerHeaderText = document.querySelector('.current-month')
@@ -6,4 +8,35 @@ const nextMonthButton = document.querySelector(".next-month-button")
 
 datePickerButton.addEventListener('click',()=>{
     datePicker.classList.toggle('show')
+
+    const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+    setupDatePicker(selectedDate)
+
 })
+
+
+function setDate(date) { 
+    datePickerButton.innerText = format(date , "MMMM do , yyyy")
+    datePickerButton.dataset.selectedDate = getUnixTime(date)
+ }
+
+
+function setupDatePicker(selectedDate){
+
+datePickerHeaderText.innerText = format(selectedDate, 'MMMM - yyyy')
+setupMonthButtons(selectedDate)
+
+}
+
+
+
+function setupMonthButtons(selectedDate){
+    nextMonthButton.addEventListener('click',()=>{
+        setupDatePicker(addMonths(selectedDate,1))
+    },{once:true})
+    previousMonthButton.addEventListener('click',()=>{
+        setupDatePicker(subMonths(selectedDate,1))
+    })
+}
+
+ setDate(new Date())
