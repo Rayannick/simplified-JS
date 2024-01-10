3de843d86b789407033dbda4f63cfbf973e23a07
Kibrya <efte7123@gmail.com>
2024-01-11 00:49:25 +0600

    update

--- project/simple_date-picker/script.js
+++ project/simple_date-picker/script.js
@@ -1,4 +1,4 @@
-import { addMonths, eachDayOfInterval, endOfMonth, endOfWeek, format, fromUnixTime, getUnixTime, startOfMonth, startOfWeek, subMonths } from "date-fns"
+import { addMonths, eachDayOfInterval, endOfMonth, endOfWeek, format, fromUnixTime, getDate, getUnixTime, isSameDay, isSameMonth, startOfMonth, startOfWeek, subMonths } from "date-fns"
 
 const datePickerButton = document.querySelector('.date-picker-button')
 const datePicker = document.querySelector('.date-picker')
@@ -6,11 +6,12 @@ const datePickerHeaderText = document.querySelector('.current-month')
 const previousMonthButton = document.querySelector(".prev-month-button")
 const nextMonthButton = document.querySelector(".next-month-button")
 let currentDate = new Date()
+const dateGrid = document.querySelector('.date-picker-grid-dates')
 
 datePickerButton.addEventListener('click', () => {
     datePicker.classList.toggle('show')
-
     const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+
     setupDatePicker(selectedDate)
     currentDate = selectedDate
 
@@ -23,11 +24,11 @@ function setDate(date) {
 }
 
 
-function setupDatePicker() {
+function setupDatePicker(selectedDate) {
 
     datePickerHeaderText.innerText = format(currentDate, 'MMMM - yyyy')
     // setupMonthButtons(selectedDate)
-
+setupDates(selectedDate)
 
 }
 
@@ -36,29 +37,48 @@ function setupDates(selectedDate) {
     const lastWeekend = endOfWeek(endOfMonth(currentDate))
     const dates = eachDayOfInterval({start : firstWeekStart, end : lastWeekend})
 
+    dateGrid.innerHTML= ''
     dates.forEach(date =>{
-        console.log(date)
-    })
+const dateElement = document.createElement('button')
+dateElement.classList.add('date');
+dateElement.innerText = date.getDate()
+if(!isSameMonth(date,currentDate)){
+    dateElement.classList.add('date-picker-other-month-date')
+}
+if(isSameDay(date,selectedDate)){
+    dateElement.classList.add('selected')
+}
+
+dateElement.addEventListener('click',()=>{
+    setDate(date)
+    datePicker.classList.remove('show')
+})
+dateGrid.appendChild(dateElement)
+
+console.log(selectedDate) //bug
+})
 }
 
 
 // function setupMonthButtons(selectedDate) {
 nextMonthButton.addEventListener('click', () => {
-    
+    const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+
     console.log('nextbtn')
 
 
     currentDate = addMonths(currentDate,1)
-    setupDatePicker()
+    setupDatePicker(selectedDate)
 }
     // , { once: true }
 )
 previousMonthButton.addEventListener('click', () => {
+    const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
 
     console.log('prevbtn')
     
     currentDate = subMonths(currentDate,1)
-    setupDatePicker()
+    setupDatePicker(selectedDate)
 })
 // }
 
